2fde6c2b800d1af73e401b10060b114ec7d179b0
anhoder <anhoder@88.com>
2024-01-16 16:17:03 +0800

    update

--- lua/hbac/autocommands.lua
+++ lua/hbac/autocommands.lua
@@ -43,39 +43,19 @@ M.autoclose.setup = function()
 			local buffers_to_close = num_buffers - config.values.threshold
 
 			-- Buffer sorted by current > pinned > is_in_window > named > unnamed
-			table.sort(buffers, function(a, b)
-				if a == current_buf or b == current_buf then
-					return b == current_buf
-				end
-				if state.is_pinned(a) ~= state.is_pinned(b) then
-					return state.is_pinned(b)
-				end
-
-				local a_windowed = #(vim.fn.win_findbuf(a)) > 0
-				local b_windowed = #(vim.fn.win_findbuf(b)) > 0
-				if a_windowed ~= b_windowed then
-					return b_windowed
-				end
-
-				local a_unnamed = vim.api.nvim_buf_get_name(a) == ""
-				local b_unnamed = vim.api.nvim_buf_get_name(b) == ""
-				if a_unnamed ~= b_unnamed then
-					return a_unnamed
-				end
-
-				return a < b
-			end)
+			table.sort(buffers, utils.sort_by)
 
 			local reserved_num = config.values.reserved_unedited_num or 1
 			for i = 1, buffers_to_close, 1 do
 				local buffer = buffers[i]
 				if not utils.buf_autoclosable(buffer) then
-					break
+					goto continue
 				elseif reserved_num > 0 then
 					reserved_num = reserved_num - 1
 				else
 					config.values.close_command(buffer)
 				end
+				::continue::
 			end
 		end,
 	})
--- lua/hbac/utils.lua
+++ lua/hbac/utils.lua
@@ -21,4 +21,29 @@ M.buf_autoclosable = function(bufnr)
 	return true
 end
 
+-- Buffer sorted by current > pinned > is_in_window > named > unnamed
+M.sort_by = function(a, b)
+	local current_buf = vim.api.nvim_get_current_buf()
+	if a == current_buf or b == current_buf then
+		return b == current_buf
+	end
+	if state.is_pinned(a) ~= state.is_pinned(b) then
+		return state.is_pinned(b)
+	end
+
+	local a_windowed = #(vim.fn.win_findbuf(a)) > 0
+	local b_windowed = #(vim.fn.win_findbuf(b)) > 0
+	if a_windowed ~= b_windowed then
+		return b_windowed
+	end
+
+	local a_unnamed = vim.api.nvim_buf_get_name(a) == ""
+	local b_unnamed = vim.api.nvim_buf_get_name(b) == ""
+	if a_unnamed ~= b_unnamed then
+		return a_unnamed
+	end
+
+	return a < b
+end
+
 return M
